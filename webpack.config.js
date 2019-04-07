33d6154f7a3826565230621ec7fc72a9c066ed01
John Tendik <johntendik@hotmail.com>
2019-04-07 02:06:39 -0400

    setting up npm things

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const config = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: 'ss-react-pagination.js'
   },
   module: {
     rules: [
